c5c921f46bfca052cb6f857e115df0f489dc3c54
Karwan87 <113824233+Karwan87@users.noreply.github.com>
2024-07-26 12:26:18 +0200

    Update Header.jsx
    header created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from './header.module.css';
+import "@fontsource/montserrat";
 
 const Header = () => {
     return (
         <div className={styles.headerContainer}>
-        <div className={styles.title}>Nagłówek</div>
-        <div className={styles.title}>Nr telefonu z odnośnikiem oraz adres mailowy z odnośnikiem</div>
+            <div className={styles.title}>R</div>
+            <div className={styles.contact}>
+            <div className={styles.mail_contact}>
+        <div className={styles.title}>Email:  </div>
+        <a href="mailto:dachrserwis@o2.pl" className={styles.link}>dachrserwis@o2.pl</a>
+        </div>
+        <div className={styles.phone_contact}>
+        <div className={styles.title}>Kontakt:</div>
+        <a href="tel:+48605104555" className={styles.link}>+48-605-104-555</a>
+        </div>
+        </div>
         </div>
     );
 };
