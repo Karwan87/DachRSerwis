b82b824245e54492cae3eb54fba6c6edb343677f
Karwan87 <113824233+Karwan87@users.noreply.github.com>
2024-08-12 11:06:58 +0200

    Update App.jsx
    Auto-scrolling after choosing option on nav with routing change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
 import Main from './Main/Main'
 import Navigation from './Navigation/Navigation'
 import Portfolio from './Portfolio/Portfolio'
@@ -9,14 +9,28 @@ import Contact from './Contact/Contact'
 // import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import "@fontsource/montserrat";
+import styles from './VAR/styles.module.css'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 export const App = () => {
   return (
     <Router basename="/dachrserwis">
     <div>
+      <ScrollToTop/>
       <Main/>
-
-            <Navigation/>
+      <div className={styles.layout}>
+      <Header/>
+      <Navigation/>
+            </div>
             <Routes>
               <Route path="/" element={<Main/>}/>
               <Route path="/portfolio" element={<Portfolio />} />
@@ -24,7 +38,7 @@ export const App = () => {
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact /> } />
             </Routes>
-            <Header/>
+           
             {/* <Footer/> */}
     </div></Router>
   );
