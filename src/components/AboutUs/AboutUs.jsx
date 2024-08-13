fffb49edb954b44f650a2e9b2c1ca47ba43aef5a
Karwan87 <113824233+Karwan87@users.noreply.github.com>
2024-08-13 11:45:26 +0200

    AboutUs updated
    reedited box-sizes
    reedited vuaslising reasons why to choose this company

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -17,22 +17,38 @@ const AboutUs = () => {
   <h2>Twoje Zaufane Źródło Profesjonalnych Usług Dekarskich i Ciesielskich</h2>
   <p>Z dumą przedstawiamy firmę Dach R Serwis, która od ponad 20 lat dostarcza najwyższej jakości usługi dekarskie i ciesielskie. Nasza firma łączy w sobie doświadczenie, precyzję oraz pasję do wykonywania prac na najwyższym poziomie. Dzięki naszemu zaangażowaniu i indywidualnemu podejściu do każdego projektu, zyskaliśmy zaufanie setek klientów.</p>
   <h3>Dlaczego Warto Wybrać Dach R Serwis</h3>
-  <div className={styles.box_container}>
-  <div className={styles.box_experience}>
-    <div className={styles.box_description}>DOŚWIADCZENIE</div>
-    <div className={styles.box_content}>20 lat w branży dekarskiej i ciesielskiej</div>
+  <div className={styles.aboutus_container}>
+  <div className={styles.box}>
+    <div className={styles.box_title}>DOŚWIADCZENIE</div>
+    <div className={styles.box_content}>
+    <div className={styles.experience_icon}>icon.svg
     </div>
-  <div className={styles.box_quality}>
-    <div className={styles.box_description}>JAKOŚĆ</div>
-    <div className={styles.box_content}>Stosowanie najlepszych matreriałów i najnowszej technologii</div>
+    <div className={styles.box_description}>20 lat w branży dekarskiej i ciesielskiej</div>
     </div>
-  <div className={styles.box_professionalism}>
-    <div className={styles.box_description}>PROFESJONALIZM</div>
-    <div className={styles.box_content}>Zespół wykwalifikowanych specjalistów, którzy dbają o każdy detal</div>
     </div>
-  <div className={styles.box_trust}>
-    <div className={styles.box_description}>ZAUFANIE</div>
-    <div className={styles.box_content}>Setki zadowolonych klientów i udanych realizacji</div>
+    <div className={styles.box}>
+    <div className={styles.box_title}>JAKOŚĆ</div>
+    <div className={styles.box_content}>
+    <div className={styles.quality_icon}>icon.svg
+    </div>
+    <div className={styles.box_description}>Stosowanie najlepszych matreriałów i najnowszej technologii</div>
+    </div>
+    </div>
+    <div className={styles.box}>
+    <div className={styles.box_title}>PROFESJONALIZM</div>
+    <div className={styles.box_content}>
+    <div className={styles.professionalism_icon}>icon.svg
+    </div>
+    <div className={styles.box_description}>Zespół wykwalifikowanych specjalistów, którzy dbają o każdy detal</div>
+    </div>
+    </div>
+    <div className={styles.box}>
+    <div className={styles.box_title}>ZAUFANIE</div>
+    <div className={styles.box_content}>
+    <div className={styles.trust_icon}>icon.svg
+    </div>
+    <div className={styles.box_description}>Setki zadowolonych klientów i udanych realizacji</div>
+    </div>
     </div>
 </div>
 
